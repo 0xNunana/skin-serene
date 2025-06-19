9e7bff48ad3fe65f72ee0eb17fee013662f0b63a
0xNunana <kudayapaul@gmail.com>
2025-06-19 03:01:20 +0000

    fixed slug error

--- app/(main)/blog/[slug]/page.tsx
+++ app/(main)/blog/[slug]/page.tsx
@@ -43,8 +43,13 @@ import { Badge } from "@/components/ui/badge";
 //   };
 // }
 
-export default function BlogPostPage({ params }: { params: { slug: string } }) {
-  const post = blogPosts.find((p) => p.slug === params.slug);
+export default async function BlogPostPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  const post = blogPosts.find((p) => p.slug === slug);
 
   if (!post) {
     notFound();
